829bebcf85878eba6ade4d749513f3b8f8740e77
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2024-10-17 11:29:29 -0700

    move extension

---
+++ BotSDK/BotApiAuthenticationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace BotSDK
+{
+    public static class BotApiAuthenticationExtensions
+    {
+        static string[] validTokenIssuers = [ "https://api.botframework.com",
+                                              "https://sts.windows.net/d6d49420-f39b-4df7-a1dc-d59a935871db/",
+                                              "https://login.microsoftonline.com/d6d49420-f39b-4df7-a1dc-d59a935871db/v2.0",
+                                              "https://sts.windows.net/f8cdef31-a31e-4b4a-93e4-5f571e91255a/",
+                                              "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0" ];
+
+        public static void AddBotApiAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddMicrosoftIdentityWebApiAuthentication(configuration)
+                .EnableTokenAcquisitionToCallDownstreamApi()
+                .AddInMemoryTokenCaches();
+
+            services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidIssuers = validTokenIssuers,
+                    ValidAudience = configuration["AzureAd:ClientId"],
+                    SignatureValidator = (token, parameters) => new JsonWebToken(token),
+                };
+            });
+        }
+    }
+}
--- MyBotAPI/Program.cs
+++ MyBotAPI/Program.cs
@@ -1,6 +1,4 @@
-using Microsoft.Identity.Web;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.JsonWebTokens;
+using BotSDK;
 using MyBotAPI;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,33 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services
-    .AddMicrosoftIdentityWebApiAuthentication(builder.Configuration)
-    .EnableTokenAcquisitionToCallDownstreamApi()
-    .AddInMemoryTokenCaches();
-
-string[] validTokenIssuers = [  "https://api.botframework.com",
-                                "https://sts.windows.net/d6d49420-f39b-4df7-a1dc-d59a935871db/",
-                                "https://login.microsoftonline.com/d6d49420-f39b-4df7-a1dc-d59a935871db/v2.0",
-                                "https://sts.windows.net/f8cdef31-a31e-4b4a-93e4-5f571e91255a/",
-                                "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0" ];
-
-builder.Services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
-{
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidIssuers = validTokenIssuers,
-        ValidAudience = builder.Configuration["AzureAd:ClientId"],
-        SignatureValidator = (token, parameters) => new JsonWebToken(token),
-    };
-});
+builder.Services.AddBotApiAuthentication(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<BotResponseService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
