1d8010ed30bc16a17e11b2eb163bd3eb5b4b6ff8
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2024-10-17 06:56:52 -0700

    debug with emul

--- MyBotAPI/BotResponseService.cs
+++ MyBotAPI/BotResponseService.cs
@@ -2,18 +2,22 @@
 using Microsoft.Identity.Abstractions;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Text;
 
 namespace MyBotAPI
 {
-    public class BotResponseService(IAuthorizationHeaderProvider auth,IHttpClientFactory httpClientFactory)
+    public class BotResponseService(IAuthorizationHeaderProvider auth, IHttpClientFactory httpClientFactory)
     {
         public async Task SendResponse(string text, Activity act)
         {
             using HttpClient client = httpClientFactory.CreateClient();
 
-            string[] Scopes = ["https://api.botframework.com/.default"];
-            var token = await auth.CreateAuthorizationHeaderForAppAsync(Scopes[0]);
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Substring("Bearer ".Length));
+            if (!act.ServiceUrl.Contains("localhost"))
+            {
+                string[] Scopes = ["https://api.botframework.com/.default"];
+                var token = await auth.CreateAuthorizationHeaderForAppAsync(Scopes[0]);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Substring("Bearer ".Length));
+            }
 
             string url = $"{act.ServiceUrl}/v3/conversations/{act.Conversation.Id}/activities";
 
@@ -22,7 +26,7 @@ namespace MyBotAPI
                 ChannelId = act.ChannelId,
                 Conversation = new ConversationAccount
                 {
-                    Id = act.Conversation.Id,
+                    Id = act.Conversation.Id.Split("|")[0],
                 },
                 From = new ChannelAccount
                 {
@@ -38,10 +42,14 @@ namespace MyBotAPI
                 Type = "message"
             };
 
+
+
             using var httpRequest = new HttpRequestMessage(HttpMethod.Post, url)
             {
+                //Content = new ByteArrayContent(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(respMsg)))
                 Content = new StringContent(JsonConvert.SerializeObject(respMsg), System.Text.Encoding.UTF8, "application/json")
             };
+            httpRequest.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             using var resp = await client.SendAsync(httpRequest);
             resp.EnsureSuccessStatusCode();
         }
--- MyBotAPI/Controllers/BotController.cs
+++ MyBotAPI/Controllers/BotController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace MyBotAPI.Controllers
@@ -13,6 +14,10 @@ namespace MyBotAPI.Controllers
         [HttpPost]
         public async Task PostAsync(object body, CancellationToken cancellationToken)
         {
+            Console.WriteLine();
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(body));
+            Console.WriteLine();
+
             Activity  act = JsonConvert.DeserializeObject<Activity>(body.ToString()!)!;
             await botResponseService.SendResponse("Hello from the bot", act);
         }
